4bbac3ab0f16f9dac98ac7061962f836afd65673
Daniil <danya25022012@gmail.com>
2024-05-11 12:16:15 +0300

    fdjkl

--- DZ5.py
+++ DZ5.py
@@ -1,12 +1,19 @@
 result = []
 def divider(a, b):
-    if a < b:
-        raise ValueError
-    if b > 100:
-        raise IndexError
-    return a/b
+    try:
+        if a < b:
+            raise ValueError("a must be greater than or equal to b")
+        if b > 100:
+            raise IndexError("b must be less than or equal to 100")
+        return a/b
+    except ValueError as ve:
+        print("ValueError:", ve)
+    except IndexError as ie:
+        print("IndexError:", ie)
+    except Exception as e:
+        print("Exception occurred:", e)
 
-data = {10: 2, 2: 5, "123": 4, 18: 0, []: 15, 8 : 4}
+data = {10: 2, 2: 5, "123": 4, 18: 0, 8 : 4}
 
 for key in data:
     res = divider(key, data[key])
