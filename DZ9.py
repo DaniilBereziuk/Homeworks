8be046da17ff6912ad11d42a42ab3be1e247f8ec
Daniil <danya25022012@gmail.com>
2024-05-24 22:51:03 +0300

    gghkhgfgdasр

--- DZ9.py
+++ DZ9.py
@@ -8,12 +8,12 @@ print(dt_string)
 
 response = requests.get('https://pogoda.meta.ua/ua/Zhytomyrska/Zhytomyrskyi/Zhytomyr/')
 text = response.text
-parse = text.split('<div class="city__forecast-feels">')
+parse = text.split('<span class="graph-data__value">')
 
 result = []
 
 for item in parse:
-    for value in item.split('</div>'):
+    for value in item.split('</span>'):
         if value[1].isdigit():
             result.append(value)
 
